1022dcbcc7a411e62c1266f6ee74ad65b7fcf290
sehaj-79 <bhasin.seh79@gmail.com>
2021-12-16 18:14:02 +0530

    Initial Commit

---
+++ MUJ Unified/MUJ_UnifiedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUJ_UnifiedApp.swift
+//  MUJ Unified
+//
+//  Created by sehejbir bhasin on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MUJ_UnifiedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
