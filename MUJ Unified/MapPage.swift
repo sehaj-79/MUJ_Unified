8db73816f5bf3e976e8b630dd4dd27a4ed4fdc13
Shikhar Kumar <shikharkumar009@gmail.com>
2021-12-18 17:20:51 +0530

    Added New Page

---
+++ MUJ Unified/MapPage.swift
@@ -0,0 +1,20 @@
+//
+//  MapPage.swift
+//  MUJ Unified
+//
+//  Created by Shikhar Kumar on 18/12/21.
+//
+
+import SwiftUI
+
+struct MapPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapPage_Previews: PreviewProvider {
+    static var previews: some View {
+        MapPage()
+    }
+}
