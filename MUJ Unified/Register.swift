ab383e8872affa2e0ae1b5780b8d0539310f1ea9
Shikhar Kumar <shikharkumar009@gmail.com>
2021-12-20 17:18:25 +0530

    Register Page Design Complete

--- MUJ Unified/Register.swift
+++ MUJ Unified/Register.swift
@@ -1,31 +1,27 @@
 //
-//  Login.swift
+//  Register.swift
 //  MUJ Unified
 //
-//  Created by Shikhar Kumar on 18/12/21.
+//  Created by Shikhar Kumar on 20/12/21.
 //
 
 import SwiftUI
 
-struct Login: View {
+struct Register: View {
     
+    @State var name:String = ""
     @State var email:String = ""
     @State var password:String = ""
+    @State var cpassword:String = ""
     
     var body: some View {
         VStack {
             
-            Spacer(minLength: 150)
             
-            Text("MUJ\nUNIFIED")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .multilineTextAlignment(.center)
-            
-            Spacer(minLength: 150)
+            Spacer()
             
             HStack{
-                Text("Sign In")
+                Text("Create Account")
                     .font(.title2)
                     .fontWeight(.semibold)
                     .padding(.leading, 25.0)
@@ -33,6 +29,26 @@ struct Login: View {
                 Spacer()
             }.padding(.bottom)
 
+            
+            
+            VStack(){
+                HStack{
+                    Text("Name")
+                        .padding(.leading, 25.0)
+                    Spacer()
+                }
+                
+                TextField("Your Name", text: $name)
+                    .padding(.horizontal, 25.0)
+                    .textFieldStyle(PlainTextFieldStyle())
+                
+                Rectangle()
+                    .offset(y:-4)
+                    .frame(height: 0.5)
+                    .foregroundColor(.gray)
+                    .padding(.horizontal, 25)
+            }
+            
             
             VStack(){
                 HStack{
@@ -50,7 +66,7 @@ struct Login: View {
                     .frame(height: 0.5)
                     .foregroundColor(.gray)
                     .padding(.horizontal, 25)
-            }
+            }.padding(.top,25)
             
             
             VStack{
@@ -60,21 +76,10 @@ struct Login: View {
                     Spacer()
                 }
                 
-                HStack {
-                    SecureField("********", text: $password)
-                        .padding(.horizontal, 25.0)
-                        .textFieldStyle(PlainTextFieldStyle())
-                    
-                    Spacer()
-                    Button {
-                        
-                    } label: {
-                        Text("Forgot Pasword")
-                            .font(.footnote)
-                            .foregroundColor(Color.gray)
-                    }.padding(.trailing,25)
-                }
-                
+                SecureField("********", text: $password)
+                    .padding(.horizontal, 25.0)
+                    .textFieldStyle(PlainTextFieldStyle())
+                                
                 Rectangle()
                     .offset(y:-4)
                     .frame(height: 0.5)
@@ -82,12 +87,28 @@ struct Login: View {
                     .padding(.horizontal, 25)
             }.padding(.top,25)
             
-
+            VStack{
+                HStack{
+                    Text("Re-enter Password")
+                        .padding(.leading, 25.0)
+                    Spacer()
+                }
+                
+                SecureField("********", text: $cpassword)
+                    .padding(.horizontal, 25.0)
+                    .textFieldStyle(PlainTextFieldStyle())
+                                
+                Rectangle()
+                    .offset(y:-4)
+                    .frame(height: 0.5)
+                    .foregroundColor(.gray)
+                    .padding(.horizontal, 25)
+            }.padding(.top,25)
             
             Button {
                 
             } label: {
-                Text("Sign In")
+                Text("Sign Up")
                     .font(.title3)
                     .frame(width: 300, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     .background(Color.black)
@@ -98,14 +119,14 @@ struct Login: View {
 
            
             HStack {
-                Text("Don't have an Account?")
+                Text("Already have an Account?")
                     .font(.footnote)
                     .foregroundColor(Color.gray)
                 
                 Button {
                     
                 } label: {
-                    Text("Sign Up")
+                    Text("Sign In")
                         .font(.footnote)
                         .foregroundColor(Color.gray)
                         .bold()
@@ -116,10 +137,8 @@ struct Login: View {
     }
 }
 
-struct Login_Previews: PreviewProvider {
+struct Register_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            Login()
-        }
+        Register()
     }
 }
