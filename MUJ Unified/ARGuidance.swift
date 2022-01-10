02ddb9fd3e9b414e60b7c4062cf2054ff57ff0d8
Shikhar Kumar <shikharkumar009@gmail.com>
2022-01-11 01:34:43 +0530

    ARGuidance Progress

--- MUJ Unified/ARGuidance.swift
+++ MUJ Unified/ARGuidance.swift
@@ -7,11 +7,30 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ARGuidance: View {
     
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack(alignment: .bottom){
+            ARViewContainer().edgesIgnoringSafeArea(.all)
+            
+            
+            HStack{
+                Button {
+                    print("checkmark pressed")
+                } label: {
+                    Text("Place")
+                        .frame(width: 60, height: 60)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(30)
+                        .padding(20)
+                        .opacity(0.75)
+                }
+
+            }
+        }
     }
 }
 
@@ -19,9 +38,27 @@ struct ARViewContainer : UIViewRepresentable{
     
     func makeUIView(context: Context) -> some UIView {
         let arView = ARView(frame: .zero)
+        
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh){
+            config.sceneReconstruction = .mesh
+        }
+        
+        arView.session.run(config)
+        
         return arView
     }
     func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+        let modelEntity = try! ModelEntity.loadModel(named: "arrow.usdz")
+        let anchorEntity = AnchorEntity(plane: .any)
+        anchorEntity.addChild(modelEntity)
+        
+        uiView.scene.addAnchor(anchorEntity)
+        
         
     }
 }
